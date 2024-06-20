9b938d4a2837fd352fe334f95283fac2f7792cae
Maqlogemink <166916142+Maqlogemink@users.noreply.github.com>
2024-06-20 22:12:48 +0700

    Update Project AkhirPBO.cpp

--- Project AkhirPBO.cpp
+++ Project AkhirPBO.cpp
@@ -10,7 +10,7 @@ class Perusahaan {
         long unsigned int Valuasi;
         
         
-    `   // Getter
+        // Getter
         string GetNama () const {
             
             return Nama;
@@ -74,13 +74,36 @@ class Department : public Perusahaan {
         string NamaDept;
         unsigned int BanyakAnggota;
         long unsigned int Anggaran ;
-        
+    
+        // Getter
+        string GetNamaDept () const {
+            
+            return NamaDept;
+        }
+        unsigned int GetBanyakAnggota () const {
+            
+            return BanyakAnggota;
+        }
+        long unsigned int GetAnggaran () const {
+            
+            return Anggaran;
+        }
+
     public :
-    
+        // setter 
         
-        
-    
-    
+        string SetNama (string __NamaDept)  {
+            NamaDept = __NamaDept;
+            return NamaDept;
+        }
+        unsigned int SetBanyakAnggota(unsigned int __BanyakAnggota)  {
+            BanyakAnggota = __BanyakAnggota;
+            return BanyakAnggota;
+        }
+        long unsigned int SetAnggaran (long unsigned int __Anggaran)  {
+            Anggaran = __Anggaran;
+            return Anggaran;
+        }
     
     
     
@@ -97,11 +120,70 @@ class Department : public Perusahaan {
 class Karyawan {
     private:
             
-            string NamaKaryawan, Jabatan, 
-            unsigned int Gaji,Umur,LamaBekerja;
-            char JenisKelamin;
+        string NamaKaryawan, Jabatan; 
+        unsigned int Gaji,Umur,LamaBekerja;
+        char JenisKelamin;
+
+        // Getter
+        string GetNamaKaryawan () const {
             
-    
+            return NamaKaryawan;
+        }
+        string GetJabatan() const {
+            
+            return Jabatan;
+        }
+        unsigned int GetGaji () const {
+            
+            return Gaji;
+        }
+        
+        unsigned int GetUmur() const {
+            
+            return Umur;
+        }
+        
+        unsigned int GetLamaBekerja () const {
+            
+            return LamaBekerja;
+        }
+
+        char GetJenisKelamin() const{
+
+            return JenisKelamin;
+        }
+    public: 
+        // setter 
+        
+        string SetNamaKaryawan (string __NamaKaryawan)  {
+            NamaKaryawan = __NamaKaryawan;
+            return NamaKaryawan;
+        }
+        string SetJabatan(string __Jabatan)  {
+            Jabatan = __Jabatan;
+            return Jabatan;
+        }
+        unsigned int SetGaji (unsigned int __Gaji)  {
+            Gaji = __Gaji;
+            return Gaji;
+        }
+        
+        unsigned int SetUmur(unsigned int __Umur)  {
+            Umur = __Umur;
+            return Umur;
+        }
+        
+        unsigned int SetLamaBekerja (long unsigned int __LamaBekerja)  {
+            LamaBekerja = __LamaBekerja;
+            return LamaBekerja;
+        }
+
+        char SetJenisKelamin(char __JenisKelamin){
+            JenisKelamin = __JenisKelamin;
+            return JenisKelamin;
+        }
+
+            
     
     
 };
