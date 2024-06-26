4da6f28f35b69a27e5f35437a2aed7725250a112
Maqlogemink <166916142+Maqlogemink@users.noreply.github.com>
2024-06-26 08:56:06 +0700

    Update Data_pekerja.cpp

--- Data_pekerja.cpp
+++ Data_pekerja.cpp
@@ -1,6 +1,9 @@
 # include "Deklarasi_projek.h"
 # include "Defenisi_projek.cpp"
+# include <fstream>
+# include <sstream>
 # include <iostream>
+# include <vector>
 
 
 class Pengembang : public Karyawan {
@@ -31,7 +34,7 @@ class Keuangan : public Karyawan {
 
 class Peneliti : public Karyawan {
 
-    
+    public:
      void Jobdesc () {
         cout << NamaKaryawan << " pada departmen " << NamaDept << " Sebagai peneliti teknologi pangan perusahaan" << endl;;
     }
@@ -39,12 +42,62 @@ class Peneliti : public Karyawan {
 
 
 };
+bool readCSV(const string& filename, Peneliti& peneliti) {
+    ifstream file(filename);
+
+    if (!file.is_open()) {
+        cerr << "Tidak bisa membuka file tersebut!" << endl;
+        return false;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string nama, jabatan, gaji, umur, lamaBekerja, jenisKelamin;
+
+        getline(ss, nama, ';');
+        getline(ss, jabatan, ';');
+        getline(ss, gaji, ';');
+        getline(ss, umur, ';');
+        getline(ss, lamaBekerja, ';');
+        getline(ss, jenisKelamin, ';');
+
+        peneliti.SetNamaKaryawan(nama);
+        peneliti.SetJabatan(jabatan);
+        peneliti.SetGaji(stoi(gaji));
+        peneliti.SetUmur(stoi(umur));
+        peneliti.SetLamaBekerja(stoi(lamaBekerja));
+        peneliti.SetJenisKelamin(jenisKelamin[0]);
+        
+    }
+
+    file.close();
+    return true;
+}
+
+
+
 
 int main () {
 
-
-
+    Pengembang pengembang01;
+    Pemasaran pemasaran01;
+    Keuangan keuangan01;
     Peneliti peneliti01;
 
-    peneliti01.SetNamaKaryawan("Jayadi");
+      if (readCSV("data.csv", peneliti01)) {
+        peneliti01.Jobdesc();
+    } else {
+        cout << "Gagal membaca data dari file CSV." << endl;
+    }
+
+    // peneliti01.SetNamaKaryawan("Jayadi");
+    // peneliti01.SetJabatan("Peneliti");
+    // peneliti01.SetGaji(2000000);
+    // peneliti01.SetUmur(25);
+    // peneliti01.SetLamaBekerja(3);
+    // peneliti01.SetJenisKelamin('M');
+
+    
+    
 }
